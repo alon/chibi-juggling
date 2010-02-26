a5d6d82a9ef4afb521eaddb3cb2d20728b85b954
Alon <alonlevy1@gmail.com>
2010-02-26 21:24:20 +0200

    added caching of wav files, and playback using gstreamer (ugly right now - recreating a pipeline everytime..)

--- pc/speaker.py
+++ pc/speaker.py
@@ -1,15 +1,37 @@
 #!/usr/bin/python
 
 import os
+
+import gst
+
 from reader import stationary_factory
 from sp import throws
+
+def make_wav(num):
+    filename = 'wav/%s.wav' % num
+    if not os.path.exists(filename):
+        os.system('flite -t %s -o %s.wav' % (num, num))
+    assert(os.path.exists(filename))
+    return filename
+
+def make_playbin(num):
+    filename = make_wav(num)
+    p=gst.Pipeline("player")
+    s=gst.element_factory_make("playbin", "stream") 
+    s.set_property("uri", "file:///home/alon/accounts/linkedin/1.wav")
+    p.add(s)
+    return p
+
+def play(num):
+    p = make_playbin(num)
+    p.set_state(gst.STATE_PLAYING)
 
 def main():
     stationary = stationary_factory()
     for throw_num, thrown in throws(stationary.accelerations()):
         print throw_num, thrown
         if thrown:
-            os.system("flite -t %s &" % throw_num)
+            play(throw_num)
 
 if __name__ == '__main__':
     main()
