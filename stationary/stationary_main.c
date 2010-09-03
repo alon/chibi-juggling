02b31bd8477922b43be5acd00e2c6960b2a3b5b5
Alon Levy <alevy@redhat.com>
2010-09-03 12:35:49 +0300

    add braces to single line blocks in stationary

--- stationary/stationary_main.c
+++ stationary/stationary_main.c
@@ -20,6 +20,7 @@ static U16 acc_X = 0, acc_Y = 0, acc_Z = 0;
 static bool print_adxl = false;
 static bool do_receive = true;
 
+
 /**************************************************************************/
 /*!
     This is the main function. Duh.
@@ -37,7 +38,8 @@ int main()
     // isn't that already
     U8 old_addr = chb_get_short_addr();
     if (old_addr != STATIONARY_SHORT_ADDRESS) {
-        printf_P(PSTR("Changing short address from %d to %d\n"), old_addr, STATIONARY_SHORT_ADDRESS);
+        printf_P(PSTR("Changing short address from %d to %d\n"),
+                      old_addr, STATIONARY_SHORT_ADDRESS);
         chb_set_short_addr(STATIONARY_SHORT_ADDRESS);
     }
 
@@ -54,25 +56,35 @@ int main()
         {
             rx_data.len = chb_read(&rx_data);
             // check also for expected length + 2 (known chibi bug)
-            if ((rx_data.len != ADXL_PACKET_LENGTH && rx_data.len != ADXL_PACKET_LENGTH + 2) || rx_data.data[0] != ADXL_MAGIC_BYTE_1
-                        || rx_data.data[1] != ADXL_MAGIC_BYTE_2) {
-                if (rx_data.len != ADXL_PACKET_LENGTH && rx_data.len != ADXL_PACKET_LENGTH + 2)
+            if ((rx_data.len != ADXL_PACKET_LENGTH &&
+                 rx_data.len != ADXL_PACKET_LENGTH + 2)
+               || rx_data.data[0] != ADXL_MAGIC_BYTE_1
+               || rx_data.data[1] != ADXL_MAGIC_BYTE_2) {
+                if (rx_data.len != ADXL_PACKET_LENGTH &&
+                    rx_data.len != ADXL_PACKET_LENGTH + 2) {
                     printf_P(PSTR("Bad packet length, "));
-                if (rx_data.data[0] != ADXL_MAGIC_BYTE_1)
+                }
+                if (rx_data.data[0] != ADXL_MAGIC_BYTE_1) {
                     printf_P(PSTR("Bad first byte, "));
-                if (rx_data.data[1] != ADXL_MAGIC_BYTE_2)
+                }
+                if (rx_data.data[1] != ADXL_MAGIC_BYTE_2) {
                     printf_P(PSTR("Bad second byte, "));
+                }
                 printf_P(PSTR("Non adxl (src %02X), #%d:"),
-                    rx_data.src_addr, rx_data.len); //rx_data.data);
+                         rx_data.src_addr, rx_data.len); //rx_data.data);
                 for (U8 i=0; i<rx_data.len; i+=2) {
-                    if (i+1 < rx_data.len)
-                        printf_P(PSTR("%X, "), rx_data.data[i] + (rx_data.data[i+1]<<8));
-                    else // last byte in an odd length string
+                    if (i+1 < rx_data.len) {
+                        printf_P(PSTR("%X, "),
+                                 rx_data.data[i] + (rx_data.data[i+1]<<8));
+                    } else { // last byte in an odd length string
                         printf_P(PSTR("%X, "), rx_data.data[i]);
+                    }
                 }
                 printf_P(PSTR("\nStopping Receive\n"));
                 do_receive = false;
-            } else { // assume this is our juggling friend, interpret as three axes accel:
+            } else {
+                // assume this is our juggling friend,
+                // interpret as three axes accel:
                 acc_X = (rx_data.data[ADXL_MAGIC_LENGTH+1] << 8)
                        + rx_data.data[ADXL_MAGIC_LENGTH+0];
                 acc_Y = (rx_data.data[ADXL_MAGIC_LENGTH+3] << 8)
@@ -81,8 +93,9 @@ int main()
                        + rx_data.data[ADXL_MAGIC_LENGTH+4];
             }
             pcb->data_rcv = false;
-            if (print_adxl)
+            if (print_adxl) {
                 printf_P(PSTR("got %d, %d, %d\n"), acc_X, acc_Y, acc_Z);
+            }
             // clear the rx flag and reset the buffer (as per chb_demo.c)
             //chb_clr(); // seems to be removed 2010-04-08_chibi_v0.8
         }
